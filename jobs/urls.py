9a3a1a1ce6e28ea65c07eb85626d6d72251a6d30
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-02-18 14:26:05 +0700

    add urls

---
+++ jobs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('employers/', views.EmployerListCreate.as_view(), name='employer-list'),
+    path('employers/<int:pk>/', views.EmployerRetrieveUpdateDestroy.as_view(), name='employer-detail'),
+    path('jobs/', views.JobListCreate.as_view(), name='job-list'),
+    path('jobs/<int:pk>/', views.JobRetrieveUpdateDestroy.as_view(), name='job-detail'),
+    path('jobs/<int:job_pk>/applications/', views.ApplicationListCreate.as_view(), name='application-list'),
+    path('jobs/<int:job_pk>/applications/<int:pk>/', views.ApplicationRetrieveUpdateDestroy.as_view(), name='application-detail'),
+]
